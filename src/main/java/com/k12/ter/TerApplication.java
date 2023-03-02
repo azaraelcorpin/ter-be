ce64ce18f42a8005384b2c5769485b0adea7016d
azaraelcorpin <azarael.corpin@msugensan.edu.ph>
2023-03-02 12:27:54 +0800

    git ignore log

--- src/main/java/com/k12/ter/TerApplication.java
+++ src/main/java/com/k12/ter/TerApplication.java
@@ -3,11 +3,19 @@ package com.k12.ter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 @SpringBootApplication
 public class TerApplication {
 
+	public static final Logger LOGGER = LogManager.getLogger(TerApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(TerApplication.class, args);
+
+		LOGGER.info("Info level log message");
+        LOGGER.debug("Debug level log message");
+        LOGGER.error("Error level log message");
 	}
 
 }
